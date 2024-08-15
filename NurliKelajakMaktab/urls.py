337897f33b51033cd1014f0a706acdbe7475e2a5
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-08-15 15:59:19 +0500

    update

--- NurliKelajakMaktab/urls.py
+++ NurliKelajakMaktab/urls.py
@@ -18,10 +18,50 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import pandas as pd
+from django.http import HttpResponse
+from django.views import View
+from django.apps import apps
+
+
+class ExportAllDataView(View):
+    def get(self, request, *args, **kwargs):
+        # Get all models
+        models = apps.get_models()
+
+        # Create a Pandas Excel writer object
+        with pd.ExcelWriter('database_dump.xlsx', engine='openpyxl') as writer:
+            # Iterate over each model
+            for model in models:
+                # Query all data from the model
+                data = model.objects.all().values()
+
+                # If there's no data, skip
+                if not data:
+                    continue
+
+                # Convert the data to a DataFrame
+                df = pd.DataFrame(list(data))
+
+                # Write the DataFrame to the Excel file with the model name as the sheet name
+                df.to_excel(writer, sheet_name=model._meta.model_name, index=False)
+
+        # Read the Excel file into memory
+        with open('database_dump.xlsx', 'rb') as f:
+            excel_data = f.read()
+
+        # Create the HTTP response
+        response = HttpResponse(excel_data,
+                                content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=database_dump.xlsx'
+
+        return response
+
 
 urlpatterns = [
     path('', admin.site.urls),
     path('/api/v1/olimpiada25082024/'),
+    path('export-all-data/', ExportAllDataView.as_view(), name='export-all-data'),
 ]
 
 if settings.DEBUG:
