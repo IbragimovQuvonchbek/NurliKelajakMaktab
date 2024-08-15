c80b7f7c50b69fb8fa091c6a0558754910a1d1f3
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-08-15 16:41:55 +0500

    update

--- NurliKelajakMaktab/urls.py
+++ NurliKelajakMaktab/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import pandas as pd
@@ -60,7 +60,7 @@ class ExportAllDataView(View):
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('/api/v1/olimpiada25082024/'),
+    path('/api/v1/olimpiada25082024/', include('olympia.urls')),
     path('export-all-data/', ExportAllDataView.as_view(), name='export-all-data'),
 ]
 
