515fd0b9db12ee21fa41eab59537e38f69abb0a4
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-08-15 17:12:52 +0500

    update

--- NurliKelajakMaktab/urls.py
+++ NurliKelajakMaktab/urls.py
@@ -23,34 +23,33 @@ from django.http import HttpResponse
 from django.views import View
 from django.apps import apps
 
-
 class ExportAllDataView(View):
     def get(self, request, *args, **kwargs):
-        # Get all models
         models = apps.get_models()
 
-        # Create a Pandas Excel writer object
         with pd.ExcelWriter('database_dump.xlsx', engine='openpyxl') as writer:
-            # Iterate over each model
             for model in models:
-                # Query all data from the model
-                data = model.objects.all().values()
+                try:
+                    data = model.objects.all().values()
+                    if not data:
+                        continue
 
-                # If there's no data, skip
-                if not data:
+                    # Convert data to a DataFrame
+                    df = pd.DataFrame(list(data))
+
+                    # Convert timezone-aware datetimes to timezone-naive datetimes
+                    for col in df.select_dtypes(include=['datetimetz']).columns:
+                        df[col] = df[col].apply(lambda x: x.tz_convert(None) if x.tzinfo is not None else x)
+
+                    df.to_excel(writer, sheet_name=model._meta.model_name, index=False)
+
+                except Exception as e:
+                    print(f"Error processing model {model._meta.model_name}: {e}")
                     continue
 
-                # Convert the data to a DataFrame
-                df = pd.DataFrame(list(data))
-
-                # Write the DataFrame to the Excel file with the model name as the sheet name
-                df.to_excel(writer, sheet_name=model._meta.model_name, index=False)
-
-        # Read the Excel file into memory
         with open('database_dump.xlsx', 'rb') as f:
             excel_data = f.read()
 
-        # Create the HTTP response
         response = HttpResponse(excel_data,
                                 content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename=database_dump.xlsx'
@@ -59,9 +58,9 @@ class ExportAllDataView(View):
 
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('api/v1/olimpiada25082024/', include('olympia.urls')),
     path('export-all-data/', ExportAllDataView.as_view(), name='export-all-data'),
+    path('', admin.site.urls),
 ]
 
 if settings.DEBUG:
