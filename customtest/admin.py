0f043c7f7719556e11c66d4ddfd5fb129d730d67
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-08-23 13:31:18 +0500

    update

--- customtest/admin.py
+++ customtest/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Results, Tests
+from .models import Results, Tests, TestFile
 
 admin.site.register(Tests)
 admin.site.register(Results)
+admin.site.register(TestFile)
