61739c816861a3cfa5e4bf6701c03c8ef624a8ba
Ibragimov Quvonchbek <ibragimov@ibragimov-quvonchbek.www.mobile.de>
2024-09-17 10:45:41 +0500

    update

--- courseIT/models.py
+++ courseIT/models.py
@@ -12,9 +12,8 @@ class Student(models.Model):
     first_name = models.CharField(max_length=255, blank=False, verbose_name="Ism")
     last_name = models.CharField(max_length=255, blank=False, verbose_name="Familiya")
     grade = models.PositiveSmallIntegerField(blank=False, verbose_name="Sinf")
-    group = models.PositiveSmallIntegerField(blank=False, default=1)
-    id_unique = models.CharField(max_length=255, default=lambda: Student.generate_unique_code(), unique=True,
-                                 verbose_name="O'quvchi ID")
+    group = models.PositiveSmallIntegerField(blank=False, default=1, verbose_name="Gruh raqami")
+    id_unique = models.CharField(max_length=255, default=lambda: Student.generate_unique_code(), unique=True, verbose_name="O'quvchi ID")
 
     @staticmethod
     def generate_unique_code():
